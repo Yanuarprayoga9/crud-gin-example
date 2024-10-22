812c4e7cf9af6eb567cc11823a5881f9b10eab69
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2024-10-22 15:47:42 +0700

    feat: setting up cors

--- cmd/main.go
+++ cmd/main.go
@@ -5,41 +5,50 @@ import (
 	"day1/handler"
 	"day1/repository"
 	"day1/service"
-	"log"
-
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"log"
+	"time"
 )
 
 func main() {
-    if err := godotenv.Load(); err != nil {
-        log.Fatal("Error loading .env file")
-    }
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
 	db := db.ConnectDb()
 
+	// Initialize repository, service, and handler
+	userRepo := repository.NewUserRepository(db)
+	userService := service.NewUserService(userRepo)
+	userHandler := handler.NewUserHandler(userService)
 
-    // Initialize repository, service, and handler
-    userRepo := repository.NewUserRepository(db)
-    userService := service.NewUserService(userRepo)
-    userHandler := handler.NewUserHandler(userService)
+	// Setup Gin router
+	r := gin.Default()
 
-    // Setup Gin router
-    r := gin.Default()
+	// Setup CORS middleware
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000", "http://localhost:5173"}, // Allow only from these origins
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},                   // Allow only specific HTTP methods
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},        // Allow specific headers
+		ExposeHeaders:    []string{"Content-Length"},                                 // Expose these headers to the frontend
+		AllowCredentials: true,                                                       // Allow cookies and other credentials
+		MaxAge:           12 * time.Hour,                                             // Cache preflight requests
+	}))
+	// Define routes
+	r.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "ping",
+			"status":  "success",
+		})
+	})
 
-    // Define routes
-    r.GET("/ping", func(ctx *gin.Context) {
-        ctx.JSON(200, gin.H{
-            "message": "ping",
-            "status":  "success",
-        })
-    })
+	r.POST("/users", userHandler.CreateUser)
+	r.GET("/users", userHandler.GetAllUsers)
+	r.GET("/users/:id", userHandler.GetUserByID)
+	r.PUT("/users/:id", userHandler.UpdateUser)
+	r.DELETE("/users/:id", userHandler.DeleteUser)
 
-    r.POST("/users", userHandler.CreateUser)
-    r.GET("/users", userHandler.GetAllUsers)
-    r.GET("/users/:id", userHandler.GetUserByID)
-    r.PUT("/users/:id", userHandler.UpdateUser)
-    r.DELETE("/users/:id", userHandler.DeleteUser)
-
-    // Start the server
-    r.Run(":8000")
+	// Start the server
+	r.Run(":8000")
 }
