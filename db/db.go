6f738fcf22a5e38ebea85f3298794b0b97c1218e
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2024-10-23 18:58:38 +0700

    feat: repeat connection db

--- db/db.go
+++ db/db.go
@@ -4,6 +4,7 @@ import (
 	"day1/model/domain"
 	"log"
 	"os"
+	"time"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
@@ -15,14 +16,29 @@ type Dbinstance struct {
 
 var DB Dbinstance
 
-func ConnectDb() (*gorm.DB) {
-	dsn :=  os.Getenv("DATABASE_URL");
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
-		Logger: logger.Default.LogMode(logger.Info),
-	})
+// ConnectDb attempts to connect to the database with retry mechanism
+func ConnectDb() *gorm.DB {
+	dsn := os.Getenv("DATABASE_URL")
+	maxRetries := 5
+	retryDelay := 5 * time.Second
+
+	var db *gorm.DB
+	var err error
+
+	for i := 0; i < maxRetries; i++ {
+		db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
+			Logger: logger.Default.LogMode(logger.Info),
+		})
+		if err == nil {
+			break
+		}
+
+		log.Printf("Failed to connect to database. Retrying in %v... (%d/%d)\n", retryDelay, i+1, maxRetries)
+		time.Sleep(retryDelay)
+	}
 
 	if err != nil {
-		log.Fatal("Failed to connect to database. \n", err)
+		log.Fatal("Failed to connect to database after maximum retries. \n", err)
 		os.Exit(2)
 	}
 
@@ -35,6 +51,6 @@ func ConnectDb() (*gorm.DB) {
 	DB = Dbinstance{
 		Db: db,
 	}
-    log.Println("connected to database")
+	log.Println("connected to database")
 	return db
 }
